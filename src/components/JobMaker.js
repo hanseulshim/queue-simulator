1b9be604b7da400e85822e1b5b72abed9a16c245
hanseulshim <hanseulshim@gmail.com>
2018-01-22 06:56:31 -0500

    add dialog to handle pause button confirmation

--- src/components/JobMaker.js
+++ src/components/JobMaker.js
@@ -3,13 +3,20 @@ import TextField from 'material-ui/TextField';
 import Button from 'material-ui/Button';
 import PlayIcon from 'material-ui-icons/PlayArrow';
 import PauseIcon from 'material-ui-icons/Pause';
+import Dialog, {
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+} from 'material-ui/Dialog';
 import { style, addJobStyle, queueControlStyle, queueControlTextStyle } from '../styles/JobMakerStyles';
 
 class JobMaker extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      jobName: 'Job 1'
+      jobName: 'Job 1',
+      open: false
     };
   }
 
@@ -17,12 +24,29 @@ class JobMaker extends Component {
     this.setState({ jobName: event.target.value });
   };
 
+  handleClickOpen = () => {
+    if (this.props.queueStart) {
+      this.setState({ open: true });
+    } else {
+      this.props.toggleQueueStart();
+    }
+  };
+
+  handleClose = () => {
+    this.setState({ open: false });
+  };
+
+  handleAgree = () => {
+    this.props.toggleQueueStart();
+    this.setState({ open: false });
+  };
+
   addJob = () => {
     this.props.addJob(this.state.jobName);
   };
 
   render() {
-    const { queueStart, toggleQueueStart } = this.props;
+    const { queueStart } = this.props;
     const { jobName } = this.state;
     return (
       <div {...style}>
@@ -38,10 +62,26 @@ class JobMaker extends Component {
         </div>
         <div {...queueControlStyle}>
           <div {...queueControlTextStyle}>Pause the Queue</div>
-          <Button fab color="primary" onClick={toggleQueueStart} >
+          <Button fab color="primary" onClick={this.handleClickOpen} >
             { queueStart ? <PauseIcon /> : <PlayIcon />}
           </Button>
         </div>
+        <Dialog
+          open={this.state.open}
+          onClose={this.handleClose}
+          aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description"
+        >
+          <DialogTitle id="alert-dialog-title">Are you sure you want to lock the queue?</DialogTitle>
+          <DialogActions>
+            <Button onClick={this.handleClose} color="primary">
+              Disagree
+            </Button>
+            <Button onClick={this.handleAgree} color="primary" autoFocus>
+              Agree
+            </Button>
+          </DialogActions>
+        </Dialog>
       </div>
     );
   }
